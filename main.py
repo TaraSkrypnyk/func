76feb7e79d9ab3ace3ababd79c2a73b2f936e004
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-30 22:51:04 +0200

    Ex3 was done

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ def proste_chislo(list):
     for i in range(len(list)):
         if (list[i] % 2 != 0) and (list[i] % 3 != 0) and (list[i] % 5 != 0) and (list[i] % 7 != 0) and (list[i] != 1) or (list[i] == 2 or (list[i] == 3) or (list[i] == 5) or (list[i] == 5)):
             rez += 1
-            print(list[i])
     return rez
 
 
 if __name__ == "__main__":
     try:
         list = [1, 12, 6, 3, 19, 8, 4, 5]
+        print(proste_chislo(list))
     except Exception as e:
         print(e)
