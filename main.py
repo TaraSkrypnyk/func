a10786a120ccee68b46f8cd4e87df3d4d7a6d78f
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-30 22:33:48 +0200

    Ex2 was done

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-try:
-
-except Exception as e:
-    print(e)
+n = int(input('Vvedit chyslo: '))
+        rez = False
+        if n<0:
+            n = int(input('Vvedit dodatne chyslo: '))
+        if (n%2 != 0) and (n%3 != 0) and (n%5 != 0) and (n%7 != 0) and (n != 1) or (n == 2):
+            rez = True
+        print(rez)
