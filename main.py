971f588cd4b8425986f5583cb0d1d183d9f2d7ef
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-31 00:07:00 +0200

    Ex11 was done

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-try:
+def find_in_list(list, i=0, rez = False):
+    if i < len(list):
+        x = int(input('Vvedit chyslo dlya poshuku: '))
+        if x in list:
+            rez = True
+            return rez
+        else:
+            return rez
+        return find_in_list(list, i + 1, rez)
+    return rez
 
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    try:
+        list = [1, 42, 6, 3, -19, -8, 4, 5]
+        print(list)
+        print(find_in_list(list))
+    except Exception as e:
+        print(e)
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import unittest
-from main import func_name
+from main import find_in_list
 
 class Test(unittest.TestCase):
+    def test_returns_false_when_list_is_empty(self):
+        # Arrange
+        lst = []
+        num = 3
+
+        # Act
+        result = find_in_list(lst, i=0, rez=False)
+
+        # Assert
+        assert result == False
