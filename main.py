b9d8add6d4ebcdd87998225adbd7a8c036b91328
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-30 23:53:14 +0200

    Ex8 was done

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-try:
+def get_max_in_list(list):
+    rez = list[0]
+    for i in range(len(list)):
+        if rez < list[i]:
+            rez = list[i]
+    return rez
 
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    try:
+        list = [1, 12, 6, 3, 19, 8, 4, 5]
+        print('max in list = ', get_max_in_list(list))
+    except Exception as e:
+        print(e)
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import unittest
-from main import func_name
+from main import get_max_in_list
 
 class Test(unittest.TestCase):
+    def test_mixed_integers(self):
+        assert get_max_in_list([-1, 2, -3, 4, -5]) == 4
+        assert get_max_in_list([-10, 20, -30, 40, -50]) == 40
+        assert get_max_in_list([-100, 200, -300, 400, -500]) == 400
