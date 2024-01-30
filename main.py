488338989c31a680d6c6abad169c1feb5f35f7d2
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-31 00:09:30 +0200

    Ex12 was done

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-try:
+def get_factorial_list(list, i=0, rez_list=[]):
+    if i < len(list):
+        factorial = 1
+        while list[i] > 1:
+            factorial *= list[i]
+            list[i] -= 1
+        rez_list.append(factorial)
+        return get_factorial_list(list, i + 1, rez_list)
+    return rez_list
 
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    try:
+        list = [1, 12, 6, 3, 19, 8, 4, 5]
+        print(get_factorial_list(list))
+    except Exception as e:
+        print(e)
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import unittest
-from main import func_name
+from main import get_factorial_list
 
 class Test(unittest.TestCase):
+    def test_same_order(self):
+        assert get_factorial_list([4, 3, 2, 1]) == [24, 6, 2, 1]
