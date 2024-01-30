29ed0e508df063ee07a55848910596d972901361
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-30 23:37:31 +0200

    Ex6 was done

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-try:
+def step_list(list, step):
+    for i in range len(list):
 
-except Exception as e:
-    print(e)
+
+
+
+if __name__ == "__main__":
+    try:
+        list = [1, 12, 6, 3, 19, 8, 4, 5]
+        step=3
+        print(step_list(list, step))
+    except Exception as e:
+        print(e)
