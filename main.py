db0f16048ed4d3d6481c5106b23a61eda0070c19
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-30 23:28:56 +0200

    Ex5 was done

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-try:
+def delete_from_list(list1, list2):
+    res_list = list1 + list2
+    return res_list
 
-except Exception as e:
-    print(e)
+
+
+if __name__ == "__main__":
+    try:
+        list1 = [1, 12, 6, 3, 19, 8, 4, 5]
+        list2 = [0, -5, 15]
+        print(delete_from_list(list1, list2))
+    except Exception as e:
+        print(e)
