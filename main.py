dc5a0ab20d625dfe309643a9046256b72ef3dfe4
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-30 22:28:13 +0200

    Ex2 was done

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-try:
+def get_product_of_list(list):
+    rez = list[0]
+    for i in range(len(list)):
+        if rez > list[i]:
+            rez = list[i]
+    return rez
 
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    try:
+        list = [1, 12, 6, 3, 19, 8, 4, 5]
+        print('lower in list = ', get_product_of_list(list))
+    except Exception as e:
+        print(e)
