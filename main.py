47f82eb1ce447b4495c2df8cf2b4909d02642f73
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-30 22:45:32 +0200

    Ex3 was done

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-n = int(input('Vvedit chyslo: '))
-        rez = False
-        if n<0:
-            n = int(input('Vvedit dodatne chyslo: '))
-        if (n%2 != 0) and (n%3 != 0) and (n%5 != 0) and (n%7 != 0) and (n != 1) or (n == 2):
-            rez = True
-        print(rez)
+def proste_chislo(list):
+    rez = 0
+    for i in range(len(list)):
+        if (list[i] % 2 != 0) and (list[i] % 3 != 0) and (list[i] % 5 != 0) and (list[i] % 7 != 0) and (list[i] != 1) or (list[i] == 2 or (list[i] == 3) or (list[i] == 5) or (list[i] == 5)):
+            rez += 1
+            print(list[i])
+    return rez
+
+
+if __name__ == "__main__":
+    try:
+        list = [1, 12, 6, 3, 19, 8, 4, 5]
+    except Exception as e:
+        print(e)
