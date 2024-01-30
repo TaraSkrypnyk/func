d9bb8859683a5d70fe91233717a939f9493a7c80
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-30 23:21:47 +0200

    Ex4 was done

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-try:
+def delete_from_list(list, d):
+    rez = 0
+    i = 0
+    l=len(list)
+    while i <= l:
+        if list[i] == d:
+            list.pop(i)
+            l -= 1
+            rez += 1
+        i += 1
+    return rez, list
 
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    try:
+        list = [1, 12, 6, 3, 19, 8, 4, 5]
+        print(list)
+        d = int(input('Введіть число яке хочете видалити зі списку: '))
+        print(delete_from_list(list, d))
+    except Exception as e:
+        print(e)
