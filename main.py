240aecd3bc8fea519456ccbd9414040751ca04c6
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-31 00:03:29 +0200

    Ex10 was done

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-try:
+def get_mirror_list(list, i=0, rez_list=[]):
+    if i < len(list):
+        rez_list.append(list[(i+1) * (-1)])
+        return get_mirror_list(list, i + 1, rez_list)
+    return rez_list
 
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    try:
+        list = [1, 42, 6, 3, -19, -8, 4, 5]
+        print(get_mirror_list(list))
+    except Exception as e:
+        print(e)
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import unittest
-from main import func_name
+from main import get_mirror_list
 
 class Test(unittest.TestCase):
+    def test_string_list_input(self):
+        assert get_mirror_list(["apple", "banana", "cherry"]) == ["cherry", "banana", "apple"]
