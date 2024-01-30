d182e8ff291e8c7509164c1462e02ad359655670
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-31 00:00:43 +0200

    Ex9 was done

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-try:
+def get_count_from_list(list, i=0, par=0, nepar=0, dod=0, vid=0):
+    if i < len(list):
+        if list[i] >= 0:
+            dod +=1
+        else:
+            vid +=1
+        if list[i]%2 == 0 and list[i]>=0:
+            par +=1
+        elif list[i]%2 == 1 and list[i]>=0:
+            nepar +=1
+        return get_count_from_list(list, i + 1, par, nepar, dod, vid)
+    return 'parnyh=',  par, 'neparnyh=',nepar, 'dodatnih=', dod, 'vidyemnyh=',vid
 
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    try:
+        list = [1, 42, 6, 3, -19, -8, 4, 5]
+        print(list)
+        print(get_count_from_list(list))
+    except Exception as e:
+        print(e)
--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import unittest
-from main import func_name
+from main import get_count_from_list
 
 class Test(unittest.TestCase):
+    def test_all_numbers_count(self):
+        # Arrange
+        lst = [-1, -2, -3, 0, 1, 2, 3]
+
+        # Act
+        result = get_count_from_list(lst)
+
+        # Assert
+        assert result == ('parnyh=', 2, 'neparnyh=', 2, 'dodatnih=', 4, 'vidyemnyh=', 3)
