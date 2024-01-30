b4af4641a53df804a1a94babf9a2e01dab1fb49a
ТараСкрипник <garoldskrypnyk@gmail.com>
2024-01-30 23:47:36 +0200

    Ex7 was done

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-try:
+def get_sum_of_list(list,  rez = 0):
+    for i in range(len(list)):
+        rez += list[i]
+    return rez
 
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    try:
+        list = [1, 12, 6, 3, 19, 8, 4, 5]
+        print('sum = ', get_sum_of_list(list))
+    except Exception as e:
+        print(e)
